87c75ae87f6138f116a6b1ae0a0a8cdca15f12b8
chenyi <chenyi4@qq.com>
2020-10-21 17:23:43 +0800

    页面显示

--- 前端代码/index/src/components/DetailList.vue
+++ 前端代码/index/src/components/DetailList.vue
@@ -4,71 +4,62 @@
         <div class="cover-list">
             <div class="menus-list" ref="list">
                 <div id="box1">
-                    <div :class="{'line-one':true, 'line-change': key == ListChoose}" v-for="(item, key) in lists" :key="key">{{item.name}}</div>
+                    <div 
+                        :class="{'line-one':true, 'line-change': key == ListChoose}" 
+                        v-for="(item, key) in lists" :key="key"
+                        @click="changeListMenu(key)"
+                    >
+                        {{item.name}}
+                    </div>
+                </div>
+            </div>
+            <div class="menus-list2" ref="list2">
+                <div id="box2" v-if="secondShow">
+                    <div :class="{'line-one':true, 'line-change': key == secondListChoose}" 
+                    @click="changeSencodMenu(key)"
+                    v-for="(item, key) in secondList" 
+                    :key="key">{{item.name}}</div>
                 </div>
             </div>
         </div>
-        <!-- <div>
+        <div>
             <div class="detail-top"></div>
             <div class="detail-left"></div>
             <div class="detail-bottom"></div>
             <div class="detail-right"></div>
-        </div>  -->
+        </div> 
     </div>
 </template>
 <script>
-    var allNumber = 0;
-    function set(dom, lists, func){
-       // var start = dom.offsetTop+80;
-        var itemHeight = 49;
+    var obj1, obj2;
+    // 组件框的高度 160  全部的高度 735 
+    function set(dom, lists, itemHeight, boxHeight){
+        var start = dom.offsetTop;
+        var itemHeight = itemHeight;
         var length = lists.length;
         var topHeight = 0;
-        var eStoreY = null;
-        var isStart = false;
+        this.setTimeout = null;
         var box = dom.children[0];
+        var onceLength = (itemHeight * length - (boxHeight/2))/boxHeight;
+        this.value = 0;
+        const self = this;
         
         var changeDom = dom.children[0];
-        dom.onmousedown = function(e){
-            // console.log("点击组件");
-            isStart = true;
-            eStoreY = e.clientY;
-        }
-        document.onmouseup = function(e){
-            // console.log("鼠标放开");
-            isStart = false;
-        }
-        
         dom.onmousemove = function(e){
-            if(isStart){
-                topHeight = topHeight + (e.clientY - eStoreY)*4;
-                if(topHeight > 49){
-                    topHeight = 49;
-                }
-                box.style.top = topHeight + 'px';
-                console.log(topHeight);
-            }
+            box.style.transition = 'none';
+            clearTimeout(self.setTimeout);
+            topHeight = ((e.clientY - start) * onceLength );
+            box.style.top = -topHeight + 'px';
         }
-
         dom.onmouseout = function(e){
-
+            self.setTimeout = setTimeout(function(){
+                box.style.transition = 'top ease-in-out 0.3s';
+                topHeight = -itemHeight * self.value;
+                box.style.top = topHeight + 'px';
+                clearTimeout(self.setTimeout);
+            }, 200);
         }
-            // 
-            //     
-            //     
-            //     if(allNumber != number){
-            //         allNumber = number;
-            //     }
-            //     if(allNumber < -(length-2) || allNumber > 1 || Math.abs(exchange)< 9){
-            //         return false;
-            //     }
-            //     
-            //     func(-allNumber);
-            // }else{
-            //     console.log(exchange);
-            //     console.log(e.clientY - start);
-            // }
     }
-
     export default {
         name: 'DetailList',
         props: {
@@ -77,64 +68,120 @@
             return {
                lists: [
                    {
-                       name: "2"
+                       name: "1",
+                       childrens: [
+                           {
+                               name: "aaaa",
+                           },
+                           {
+                               name: "bbbb"
+                           },
+                           {
+                               name: "cccc"
+                           },
+                           {
+                               name: "ddddd"
+                           },
+                           {
+                               name: "eeeee"
+                           },
+                           {
+                               name: "fffff"
+                           },
+                           {
+                               name: "ggggg"
+                           }
+                       ]
                    },
                    {
-                       name: "1"
+                       name: "2",
+                       childrens: [
+                           {
+                               name: "aaaa"
+                           },
+                           {
+                               name: "bbbb"
+                           }
+                       ]
                    },
                    {
-                       name: "3"
-                   },
-                   {
-                       name: "目录4"
-                   },
-                   {
-                       name: "目录5"
-                   },
-                   {
-                       name: "目录6"
-                   },
-                   {
-                       name: "目录7"
-                   },
-                   {
-                       name: "目录8"
-                   },
-                   {
-                       name: "目录9"
-                   },
-                   {
-                       name: "目录10"
-                   },
-                   {
-                       name: "目录6"
-                   },
-                   {
-                       name: "目录7"
-                   },
-                   {
-                       name: "目录8"
-                   },
-                   {
-                       name: "目录9"
-                   },
-                   {
-                       name: "目录10111"
+                       name: "3",
+                       childrens: [
+                           {
+                               name: "aaaa"
+                           },
+                           {
+                               name: "bbbb"
+                           },
+                           {
+                               name: "aaaa"
+                           },
+                           {
+                               name: "bbbb"
+                           },
+                           {
+                               name: "aaaa"
+                           },
+                           {
+                               name: "bbbb"
+                           },
+                           {
+                               name: "aaaa"
+                           },
+                           {
+                               name: "bbbb"
+                           },
+                           {
+                               name: "aaaa"
+                           },
+                           {
+                               name: "bbbb"
+                           },
+                           {
+                               name: "aaaa"
+                           },
+                           {
+                               name: "bbbbend"
+                           }
+                       ]
+                   },{
+                       name: "4",
+                       childrens: []
                    }
                ],
-               ListChoose: 1
+               ListChoose: 0, //第一个choose
+               secondList: [], //
+               secondShow: false,
+               secondListChoose: null //第二个choose
             }
         },
         created(){
             const self = this;
             this.$nextTick(() => {
-                console.log('测试内容');
-                set(this.$refs.list, self.lists, self.changeListMenu);
+                obj1 = new set(this.$refs.list, self.lists,49, 160);
             });
         },
         methods:{
             changeListMenu(i){
+                const self = this;
                 this.ListChoose = i;
+                obj1.value = i;
+
+                this.secondShow = false;
+                var time1 = setTimeout(() => {
+                    self.secondList = self.lists[i].childrens;
+                    self.secondShow = true;
+                    clearTimeout(time1);
+                });
+                var time2 = setTimeout(() => {
+                    obj2 = new set(this.$refs.list2, self.secondList, 49, 400);
+                    clearTimeout(time2);
+                }, 100);
+            },
+            changeSencodMenu(i){
+                const self = this;
+                self.secondListChoose = i;
+                obj2.value = i;
             }
         }
     }
@@ -153,14 +200,14 @@
 .cover-list{
     width: 100%;
     height: 100%;
-    background: rgba(0,0,0,0.1);
+    background: rgba(0,0,0,0.3);
     position: absolute;
     left: 0px;
     top: 0px;
     padding: 5px;
     box-sizing: border-box;
-    .menus-list{
-        width: 25%;
+    .menus-list, .menus-list2{
+        width: calc(25% - 40px);
         max-height: 160px;
         overflow: hidden;
         position: absolute;
@@ -176,29 +223,91 @@
             position: absolute;
             right: 0px;
             top: 50%;
-            margin-top: 0px;
+            // margin-top: -55px;
+            z-index: 10;
         }
+        // &::after{
+        //     content: '';
+        //     width: 100%;
+        //     height: 100%;
+        //     position: absolute;
+        //     left: 0px;
+        //     top: 0px;
+        // }
         #box1{
             position: relative;
-            transition: top linear .35s;
+            // transition: top linear .1s;
             top: 0px;
         }
         .line-one{
             width: 100%;
-            border-bottom: 1px solid white;
-            color: white;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.8);
+            color: rgba(255, 255, 255, 0.5);
             line-height: 48px;
             padding-left: 50px;
             font-weight: bold;
             cursor: pointer;
             text-align: left;
-            transition: all ease-in-out .41s 0.2s;
+            transition: all ease-in-out .21s 0s;
+            &:hover{
+                color: rgba(255, 255, 255, 1);
+                transform: scale(1.06) rotateZ(2deg);
+                border-bottom: 1px solid rgba(255, 255, 255, 1);
+            }
         }
         .line-change{
             color: #7064a9;
             transform: scale(1.04);
+            border-bottom: 1px solid rgba(255, 255, 255, 1);
+            &:hover{
+                color: #7064a9;
+                transform: scale(1.06);
+            }
         }
     }
+    .menus-list2{
+        left: 25%;
+        height: auto;
+        max-height: 400px;
+        margin-top: -100px;
+        transform: translateY(-50%);
+        &::before{
+            // margin-top: 0px;
+            z-index: 10;
+        }
+        #box2{
+            position: relative;
+        }
+        .line-one{
+            animation: line linear 1s 1;
+            transition: transform ease-in-out 0.2s;
+            @for $i from 1 through 13{
+                &:nth-of-type(#{$i}){
+                    animation: line linear 0.2s 1 $i*0.1s;
+                }
+            }
+            &:hover{
+                animation-fill-mode: none;
+                opacity: 1;
+                color: rgba(255, 255, 255, 1);
+               
+                transform: scale(1.06) rotateZ(2deg);
+                border-bottom: 1px solid rgba(255, 255, 255, 1);
+            }
+        }
+    }
+}
+@keyframes line{
+    0% {
+      transform: rotateZ(4deg);
+      top: -10px;
+      opacity: 0.4;
+  }
+  100% {
+      transform: rotateZ(0deg);
+      top: -0px;
+      opacity: 1;
+  } 
 }
 .detail-top,
 .detail-left,

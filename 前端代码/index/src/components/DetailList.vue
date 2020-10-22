75aa49528084d6489c1978c31c3c1d64e9e7edfb
chenyi <chenyi4@qq.com>
2020-10-22 13:45:42 +0800

    页面修改

--- 前端代码/index/src/components/DetailList.vue
+++ 前端代码/index/src/components/DetailList.vue
@@ -19,6 +19,16 @@
                     @click="changeSencodMenu(key)"
                     v-for="(item, key) in secondList" 
                     :key="key">{{item.name}}</div>
+                </div>          
+            </div>
+            <div class="menus-list3" ref="list3">
+                <div id="box3" v-if="threeShow">
+                    <div :class="{'line-one':true, 'line-change': key == threeListChoose}" 
+                    v-for="(item, key) in threeList" 
+                    @click="selectThreeMenu(key)"
+                    :key="key">
+                        {{item.name}}
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,7 +41,7 @@
     </div>
 </template>
 <script>
-    var obj1, obj2;
+    var obj1, obj2, obj3;
     // 组件框的高度 160  全部的高度 735 
     function set(dom, lists, itemHeight, boxHeight){
         var start = dom.offsetTop;
@@ -39,23 +49,23 @@
         var length = lists.length;
         var topHeight = 0;
         this.setTimeout = null;
-        var box = dom.children[0];
+        this.box = dom.children[0];
         var onceLength = (itemHeight * length - (boxHeight/2))/boxHeight;
         this.value = 0;
         const self = this;
         
         var changeDom = dom.children[0];
         dom.onmousemove = function(e){
-            box.style.transition = 'none';
+            self.box.style.transition = 'none';
             clearTimeout(self.setTimeout);
             topHeight = ((e.clientY - start) * onceLength );
-            box.style.top = -topHeight + 'px';
+            self.box.style.top = -topHeight + 'px';
         }
         dom.onmouseout = function(e){
             self.setTimeout = setTimeout(function(){
-                box.style.transition = 'top linear 0.15s';
+                self.box.style.transition = 'top linear 0.15s';
                 topHeight = -itemHeight * self.value + (dom.offsetHeight/2) - itemHeight/2;
-                box.style.top = topHeight + 'px';
+                self.box.style.top = topHeight + 'px';
                 clearTimeout(self.setTimeout);
             }, 200);
         }
@@ -68,13 +78,53 @@
             return {
                lists: [
                    {
-                       name: "1",
+                       name: "目录1",
                        childrens: [
                            {
-                               name: "aaaa",
+                               name: "月运",
+                               childrens: [
+                                   {
+                                       name: "2020年1月"
+                                   },
+                                   {
+                                       name: "2020年2月"
+                                   },
+                                   {
+                                       name: "9876543"
+                                   },
+                                   {
+                                       name: "45678910"
+                                   },
+                                   {
+                                       name: "1234567"
+                                   },
+                                   {
+                                       name: "5678910"
+                                   },
+                                   {
+                                       name: "1234567"
+                                   },
+                                   {
+                                       name: "5678910"
+                                   },
+                                   {
+                                       name: "1234567"
+                                   },
+                                   {
+                                       name: "5678910"
+                                   }
+                               ]
                            },
                            {
-                               name: "bbbb"
+                               name: "bbbb",
+                               childrens: [
+                                   {
+                                       name: "1234567"
+                                   },
+                                   {
+                                       name: "2345678"
+                                   }
+                               ]
                            },
                            {
                                name: "cccc"
@@ -86,7 +136,21 @@
                                name: "eeeee"
                            },
                            {
-                               name: "fffff"
+                               name: "fffff",
+                               childrens: [
+                                   {
+                                       name: "1234567"
+                                   },
+                                   {
+                                       name: "2345678"
+                                   },
+                                   {
+                                       name: "1234567"
+                                   },
+                                   {
+                                       name: "2345678"
+                                   }
+                               ]
                            },
                            {
                                name: "ggggg"
@@ -144,7 +208,8 @@
                                name: "bbbbend"
                            }
                        ]
-                   },{
+                   },
+                   {
                        name: "4",
                        childrens: []
                    }
@@ -152,7 +217,10 @@
                ListChoose: 0, //第一个choose
                secondList: [], //
                secondShow: false,
-               secondListChoose: null //第二个choose
+               secondListChoose: null, //第二个choose
+               threeList: [], //第三个列表
+               threeShow: false,
+               threeListChoose: null //第三个choose
             }
         },
         created(){
@@ -169,7 +237,7 @@
 
                 this.secondShow = false;
                 var time1 = setTimeout(() => {
-                    self.secondList = self.lists[i].childrens;
+                    self.secondList = self.lists[i].childrens||[];
                     self.secondShow = true;
                     self.secondListChoose = null;
                     obj2 = null;
@@ -177,6 +245,7 @@
                 });
                 var time2 = setTimeout(() => {
                     obj2 = new set(this.$refs.list2, self.secondList, 49, 300);
+                    obj2.value = null;
                     clearTimeout(time2);
                 }, 100);
             },
@@ -184,6 +253,26 @@
                 const self = this;
                 self.secondListChoose = i;
                 obj2.value = i;
+
+                this.threeShow = false;
+                var time3 = setTimeout(() => {
+                    self.threeList = self.secondList[i].childrens||[];
+                    self.threeShow = true;
+                    self.threeListChoose = null;
+                    obj3 = null;
+                    clearTimeout(time3);
+                });
+
+                var time4 = setTimeout(() => {
+                    obj3 = new set(this.$refs.list3, self.threeList, 49, 300);
+                    obj3.value = null;
+                    clearTimeout(time4);
+                }, 100);
+            },
+            selectThreeMenu(i){
+                const self = this;
+                self.threeListChoose = i;
+                obj3.value = i;
             }
         }
     }
@@ -208,7 +297,7 @@
     top: 0px;
     padding: 5px;
     box-sizing: border-box;
-    .menus-list, .menus-list2{
+    .menus-list, .menus-list2, .menus-list3{
         width: calc(25% - 40px);
         max-height: 160px;
         overflow: hidden;
@@ -267,14 +356,13 @@
             }
         }
     }
-    .menus-list2{
+    .menus-list2, .menus-list3{
         left: 25%;
         height: auto;
         max-height: 300px;
         margin-top: -76px;
         transform: translateY(-50%);
         &::before{
-            // margin-top: 0px;
             z-index: 10;
         }
         #box2{
@@ -282,17 +370,18 @@
         }
         .line-one{
             animation: line linear 1s 1;
-            transition: transform ease-in-out 0.2s;
             @for $i from 1 through 13{
                 &:nth-of-type(#{$i}){
                     animation: line linear 0.2s 1 $i*0.1s;
+                    animation-fill-mode: both;
+                    transition: all ease-in-out 0.21s 0s;
                 }
             }
             &:hover{
                 animation-fill-mode: none;
+                transition: all ease-in-out 0.21s 0s;
                 opacity: 1;
                 color: rgba(255, 255, 255, 1);
-               
                 transform: scale(1.06) rotateZ(3deg);
                 border-bottom: 1px solid rgba(255, 255, 255, 1);
             }
@@ -302,6 +391,16 @@
                 color: #7064a9;
                 transform: scale(1.06);
             }
+        }
+    }
+    .menus-list3{
+        left: 50%;
+        height: auto;
+        max-height: 300px;
+        margin-top: -76px;
+        transform: translateY(-50%);
+        #box3{
+            position: relative;
         }
     }
 }

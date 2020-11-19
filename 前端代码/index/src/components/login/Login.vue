8abbbb2d54daa339b02e7529e99161ca82493509
chenyi <chenyi4@qq.com>
2020-11-19 13:53:27 +0800

    登录界面修改

--- 前端代码/index/src/components/login/Login.vue
+++ 前端代码/index/src/components/login/Login.vue
@@ -4,12 +4,25 @@
         <div class="true-login-box">
             <div class="show-box-2"></div>
             <div class="show-box-1">
-                <input class="name"/>
-                <input class="password"/>
-                <div class="submit"></div>
+                <div>
+                    <input class="name"/>
+                </div>
+                <div>
+                    <input class="password" type="password"/>
+                </div>
+                <div class="submit">登 录</div>
             </div>
             <div class="triggle-tran-1"></div>
             <div class="triggle-tran-2"></div>
+            <div class="show-logo-box">
+                <div class="circle2"></div>
+                <div class="circle1"></div>
+                <div class="circle3"></div>
+                <div class="circle4"></div>
+                <div class="circle5"></div>
+                <div class="circle6"></div>
+                <div class="circle7"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -66,13 +79,13 @@ $blueColor: #759bff;
             box-shadow: 1px 8px 20px 0px rgba(0,0,0,0.3);
             .name,.password{
                 width: 76%;
-                border:  1px solid $borderColor;
+                border:  1px solid #eaeaea;
                 position: relative;
-                top: 12px;
+                top: 25px;
                 padding: 10px 10px;
                 box-sizing: border-box;
                 border-radius: 6px;
-                padding-left: 30px;
+                padding-left: 50px;
                 z-index: 10;
                 margin-bottom: 15px;
                 transition: border 0.6s ease;
@@ -85,7 +98,13 @@ $blueColor: #759bff;
                width: 76%;
                color: white;
                background: $blueColor;
-               border-radius: 8px;
+               border-radius: 4px;
+               margin: 0 auto;
+               margin-top: 20px;
+               padding: 8px 0px;
+               position: relative;
+               top: 16px;
+               cursor: pointer;
             }
         }
         
@@ -121,6 +140,71 @@ $blueColor: #759bff;
             &::after{
                 display: none;
             }
+        }
+    }
+    .show-logo-box{
+        width: 100px;
+        height: 100px;
+        position: absolute;
+        top: 35px;
+        left: 50%;
+        margin-left: -50px;
+        // background: grey;
+        .circle1, .circle2,.circle3,.circle4,.circle6,.circle7{
+            width: 40px;
+            height: 40px;
+            background: black;
+            border-radius: 40px;
+            position: absolute;
+            left: 45px;
+            top: 20px;
+        }
+        .circle2{
+            left: 54px;
+            width: 42px;
+            height: 42px;
+            top: 19px;
+            border-radius: 42px;
+            background: $blueColor;
+        }
+        .circle3{
+            width: 6px;
+            height: 6px;
+            background: black;
+            border-radius: 6px;
+            left: 24px;
+            top: 20px;
+        }
+        .circle4{
+            width: 2px;
+            height: 2px;
+            border-radius: 2px;
+            left: 115px;
+            top: 44px;
+        }
+        .circle5{
+            width: 60px;
+            height: 60px;
+            border-radius: 60px;
+            background-image: linear-gradient(rgba(0,0,0,1),#759bff);
+            opacity: 0.5;
+            left: 130px;
+            top: 50px;
+            position: absolute;
+        }
+        .circle6{
+            width: 5px;
+            height: 5px;
+            border-radius: 5px;
+            left: 140px;
+            top: 90px;
+        }
+        .circle7{
+            width: 3px;
+            height: 3px;
+            left: 122px;
+            top: 102px;
+            background: black;
         }
     }
 }

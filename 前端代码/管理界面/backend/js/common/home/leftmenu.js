18f773fbcaef874c617e54bcbbcbb41332251596
chenyi <chenyi4@qq.com>
2020-12-04 16:28:07 +0800

    后台代码

---
+++ 前端代码/管理界面/backend/js/common/home/leftmenu.js
@@ -0,0 +1,54 @@
+//左侧二级导航菜单
+
+var lefts = [
+      {
+        name: "菜单1",
+        childrens: [
+            {
+                name: "详细1"
+            },
+            {
+                name: "详细2"
+            },
+            {
+                name: "详细3"
+            },
+            {
+                name: "详细4"
+            }
+        ]
+      },
+      {
+        name: "菜单2",
+        childrens: [
+            {
+                name: "详细1"
+            },
+            {
+                name: "详细2"
+            },
+            {
+                name: "详细3"
+            }
+        ]
+      },
+      {
+        name: "菜单3",
+        childrens: [
+            {
+                name: "详细1"
+            },
+            {
+                name: "详细2"
+            }
+        ]
+      },
+      {
+        name: "菜单4",
+        childrens: [
+            {
+                name: "详细1"
+            }
+        ]
+      }
+];
